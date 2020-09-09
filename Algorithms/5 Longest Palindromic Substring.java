88d57a020af0b14dddaf3cf509a05b79642cb448
Shatgul Ogen <54475694+shatgulogen@users.noreply.github.com>
2020-09-08 22:08:39 -0400

    Create 5 Longest Palindromic Substring.java

---
+++ Algorithms/5 Longest Palindromic Substring.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int longestPalSubstr(String str) { 
+        int n = str.length();   
+        boolean table[][] = new boolean[n][n]; 
+  
+        int maxLength = 1; 
+        for (int i = 0; i < n; ++i) 
+            table[i][i] = true; 
+  
+        int start = 0; 
+        for (int i = 0; i < n - 1; ++i) { 
+            if (str.charAt(i) == str.charAt(i + 1)) { 
+                table[i][i + 1] = true; 
+                start = i; 
+                maxLength = 2; 
+            } 
+        } 
+          
+        for (int k = 3; k <= n; ++k) { 
+
+            for (int i = 0; i < n - k + 1; ++i)  
+
+                int j = i + k - 1; 
+
+                if (table[i + 1][j - 1] && str.charAt(i) ==  
+                                          str.charAt(j)) { 
+                    table[i][j] = true; 
+  
+                    if (k > maxLength) { 
+                        start = i; 
+                        maxLength = k;
+                    } 
+                } 
+            } 
+        } 
+          
+        return maxLength;// return length of LPS 
+    } 
+}
